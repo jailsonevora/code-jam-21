9c1cc8791365c48c53243a4454e6adb42d7ac6df
jailsonevora <jailsonevora@gmail.com>
2022-03-11 14:27:08 -0100

    reversort

--- Qualification Round/1.Reversort.cpp
+++ Qualification Round/1.Reversort.cpp
@@ -1,5 +1,36 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<sstream>
 
 using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin>>t;
+    for(int i=1; i <= t; ++i){
+        
+        int n;
+        cin>>n;
+        std::cin.ignore();
+
+        int temp;
+        string line;
+        vector<int> sequence;
+
+        getline(std::cin, line);
+        istringstream ss(line);
+
+        while (ss >> temp)
+            sequence.push_back(temp);
+
+        std::cin.clear();
+    }
+    return 0;
+}
