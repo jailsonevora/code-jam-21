f5e1409e6204e2b46199b0a39662d88ae9fa0d13
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-03-15 21:36:23 -0100

    code jam

--- Qualification Round/2.Moons and Umbrellas.cpp
+++ Qualification Round/2.Moons and Umbrellas.cpp
@@ -1,13 +1,37 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<sstream>
 
 using namespace std;
+
+int moonsUmbrella(items){
+    return 0;
+}
 
 int main(){
 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    int t;
+    cin>>t;
+    for(int i=1; i <= t; ++i){
+
+        int temp;
+        string line;
+        vector<int> items;
+
+        getline(std::cin, line);
+        istringstream ss(line);
+
+        while (ss >> temp)
+            items.push_back(temp);
+
+        cout << "Case #" << i << ": " << moonsUmbrella(items) << endl;
+
+        std::cin.clear();
+    }
     
 }
