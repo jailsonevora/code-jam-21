ec91e7e32948af1e00e534e49e2a1615ff31fa28
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2022-03-17 22:51:44 -0100

    code jam

--- Qualification Round/3.Reversort Engineering.cpp
+++ Qualification Round/3.Reversort Engineering.cpp
@@ -4,3 +4,36 @@
 #include<sstream>
 
 using namespace std;
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin>>t;
+    for(int i=1; i <= t; ++i){
+        
+        int n;
+        cin>>n;
+        std::cin.ignore();
+
+        int temp;
+        string line;
+        vector<int> items;
+
+        getline(std::cin, line);
+        istringstream ss(line);
+
+        while (ss >> temp)
+            items.push_back(temp);
+
+        cout << "Case #" << i << ": " << reversort(items) << endl;
+
+        std::cin.clear();
+    }
+    return 0;
+}
